75667e5af0f5ddcb499caf183ae2cb4e9cc2efc6
tsira19 <120280898+tsira19@users.noreply.github.com>
2023-06-17 12:14:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function gamotvale (){
+    var x = document.getElementById ("input").value;
+  
+    if(x > 0){
+        window.alert("რიცხვი დადებითია !");
+    }else if(x == 0){
+        window.alert("არ შეიძლება იყოს 0-ის ტოლი");
+    }else{
+        window.alert(" რიცხვი უარყოფითია!")
+    }
+}
+function myFunction() {
+    document.getElementById("mydiv") .style.backgroundColor = "blue";
+  }
+   function myfunction_1 () {
+    document.getElementById ("mydiv") .style.backgroundColor = "yellow";
+   }
